1d101e3bf072f3e7f86a734c9ab8f56ecd7a6393
ShykhantsouIvan <ivanka6342@mail.ru>
2019-03-21 02:37:50 +0300

    improved project for working with std::regex

--- SortMethods/Regex/Regex.cpp
+++ SortMethods/Regex/Regex.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 #include <regex>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-void checkRegex(string initialString) {
-	// BLOCK A: find all substrings with "email"
-	{
-		string rx = "(.+)(email)(.+)";
-		regex rgx(rx);
-		cmatch matchRes;
+// this work on regex101.com : ((?<![\w\S\B])(([\w]{1,25}[\.\w]*)(@{1})([\w]{1,25})(\.)([\w]{1,25})))(?![\w\S\B])
 
-		cout << "The 1st test:\r\n";
-		cout << "Regular expression: \"" << rx << "\"\r\n";
+// find all substrings with "email"
+void checkRegexMatch(vector<string> strVector) {
+	string rx = "([\\w]{1,25}[\.\\w]*)(@{1})([\\w]{1,25})(\.)([\\w]{1,25})";
+	regex re(rx);
+	cmatch matchRes;
 
-		if (regex_match(initialString.c_str(), matchRes, rgx))
+	cout << "The 1st test: Match\r\n";
+	cout << "Regular expression: \"" << rx << "\"\r\n";
+
+	for (int i = 0; i < strVector.size(); i++) {
+		cout << "init string: \"" << strVector[i] << "\"\r\n";
+
+		if (regex_match(strVector[i].c_str(), matchRes, re))
 		{
-			cout << "\tThe matches: ";
+			cout << "\tThis string matches the pattern: ";
 			for (unsigned i = 0; i < matchRes.size(); ++i) {
 				cout << "\r\n\t\t[" << matchRes[i] << "] ";
 			}
 			cout << "\r\n";
 		}
 		else
-			cout << "\tNo matches\r\n";
+			cout << "\tThis string does not matches the pattern\r\n";
 	}
+	cout << "\r\n";
+}
 
-	// lambda func that parse email from BLOCK B
+// search all real emails contained in the string
+void checkRegexSearch(string initialString) {
+
+	// lambda func that parse email // now not used
 	auto f = [](string str){
 		string rx = "([\\w]{1,25}[\.\\w]*)(@{1})([\\w]{1,25})(\.)([\\w]{1,25})";
 		regex rgx(rx);
@@ -40,36 +50,44 @@ void checkRegex(string initialString) {
 		cout << "\r\n";
 	};
 
-	// BLOCK B: search all real emails contained in the string
-	{
-		// this work on regex101.com : ((?<![\w\S\B])(([\w]{1,25}[\.\w]*)(@{1})([\w]{1,25})(\.)([\w]{1,25})))(?![\w\S\B])
-		string rx = "[\\w]{1,25}[\.\\w]*@{1}[\\w]{1,25}\.[\\w]{1,25}";
-		regex rgx(rx);
-		cmatch matchRes;
+	string rx = "([\\w]{1,25}[\.\\w]*@{1}[\\w]{1,25}\.[\\w]{1,25})";
+	regex re(rx);
+	cmatch matchRes;
 
-		cout << "The 2nd test:\r\n";
-		cout << "Regular expression: \"" << rx << "\"\r\n";
-		
-		if (regex_search(initialString.c_str(), matchRes, rgx))
-		{
-			cout << "\tThe search: ";
-			for (unsigned i = 0; i < matchRes.size(); ++i) {
-				cout << "\r\n\t\t[" << matchRes[i] << "] " << endl;
-				f(matchRes[i]);
-			}
-			cout << "\r\n";
-		}
-		else
-			cout << "\tNo matches\r\n";
+	cout << "The 2nd test: Search\r\n";
+	cout << "Regular expression: \"" << rx << "\"\r\n";
+
+	while (regex_search(initialString.c_str(), matchRes, re)) {
+		for (int i = 1; i < matchRes.size(); i++)
+			std::cout << "\t\t[" << matchRes[i] << "]\r\n";
+		//f(matchRes[i]);
+		initialString = (matchRes.suffix()).str();
 	}
+
+	cout << "\r\n";
+}
+
+void checkRegexReplace(string initialString) {
+	string rx = "([\\w]{1,25}[\.\\w]*@{1}[\\w]{1,25}\.[\\w]{1,25})";
+	regex re(rx);
+	string replacement = "000";
+
+	cout << "The 3rd test: Replace\r\n";
+	cout << "Regular expression: \"" << rx << "\"\r\n";
+
+	initialString = regex_replace(initialString.c_str(), re, replacement.c_str());
+
+	cout << "Now initial string is: " << initialString << "\r\n";
 }
 
 int main() {
 	string targetSequence = "Please, choose right email: 1)\"_your.email@gmail.com\", 2)\".not_a_em@il$$\", 3)\"_person.pp@service.com\"";
-	cout << "Intial string: \"" << targetSequence << "\"\r\n";
+	vector<string> strVector = { "_your.email@gmail.com", ".not_a_em@il$$", "_person.pp@service.com" };
+	cout << "Intial string: \"" << targetSequence << "\"\r\n\r\n";
 
-	checkRegex(targetSequence);
-	cout << endl;
+	checkRegexMatch(strVector);
+	checkRegexSearch(targetSequence);
+	checkRegexReplace(targetSequence);
 	
 	system("pause");
 	return 0;
